8ab98382ee8ffacd64b610906d9a1ef07aa7ad48
Emmanuel Anzele <eanzele@gmail.com>
2019-01-02 19:58:44 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++
@@ -1,6 +0,0 @@
-describe "current_age_for_birth_year method" do
-  it "should return the current year for a person born in year 1983" do
-    age = current_age_for_birth_year(1983)
-    
-    expect(age).to eq(2003)
-  end  
